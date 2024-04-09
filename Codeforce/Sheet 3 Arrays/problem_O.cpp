62d3aee64e36f7231aa7b844476b634924adda03
mdmahfuzbipu <mdmahfuzbipu@gmail.com>
2024-04-09 16:53:42 +0600

    PROBLEM O

--- Codeforce/Sheet 3 Arrays/problem_O.cpp
+++ Codeforce/Sheet 3 Arrays/problem_O.cpp
@@ -1,22 +1,53 @@
 #include <iostream>
+ 
 using namespace std;
-int fibo(int n)
+ 
+unsigned long long fibonacci(int n)
 {
-    if(n==1 || n==2)
+    if (n == 1)
     {
-        return n-1;
+        return 0;
     }
-    else
-        return fibo(n-1)+fibo(n-2);
-
+    else if (n == 2)
+    {
+        return 1;
+    }
+ 
+    unsigned long long prevPrev = 0;
+    unsigned long long prev = 1;
+    unsigned long long current;
+ 
+    for (int i = 3; i <= n; i++)
+    {
+        current = prevPrev + prev;
+        prevPrev = prev;
+        prev = current;
+    }
+ 
+    return current;
 }
-
+ 
 int main()
 {
     int n;
-    cin>>n;
-
-    int fibo_number=fibo(n);
-
-    cout<<fibo_number<<endl;
+    cin >> n;
+ 
+    unsigned long long fiboNumber;
+ 
+    if (n == 1)
+    {
+        fiboNumber = 0;
+    }
+    else if (n == 2)
+    {
+        fiboNumber = 1;
+    }
+    else
+    {
+        fiboNumber = fibonacci(n);
+    }
+ 
+    cout << fiboNumber;
+ 
+    return 0;
 }
