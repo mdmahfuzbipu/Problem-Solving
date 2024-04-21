e8e560f93020b6c7ccb1fc17313a058f757f2f1d
mdmahfuzbipu <mdmahfuzbipu@gmail.com>
2024-04-22 00:58:12 +0600

    Problem A String

---
+++ Codeforce/Sheet 4 Strings/problem_A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+    string s,t;
+    getline(cin,s);
+    getline(cin,t);
+
+    cout<<s.length()<<" "<<t.length()<<endl;
+
+    cout<<s<<" "<<t<<endl;
+}
