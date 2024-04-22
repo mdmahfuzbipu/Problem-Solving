8457bf7af0f6c795fc5ef3700465ee346fce60f0
mdmahfuzbipu <mdmahfuzbipu@gmail.com>
2024-04-22 16:51:56 +0600

    Problem B final

--- Codeforce/Sheet 4 Strings/problem_B.cpp
+++ Codeforce/Sheet 4 Strings/problem_B.cpp
@@ -3,14 +3,22 @@
 using namespace std;
 int main()
 {
+    //better solution
     string S;
-    getline(cin,S);
+    getline(cin,S,'\\'); //third delimeter
 
-    int i=0;
+    cout<<S;
 
+//second way bad solution because of loop
+/*
+     int i=0;
+    
     while(S[i] != '\\')
     {
         cout<<S[i];
         i++;
     }
+
+*/
+   
 }
