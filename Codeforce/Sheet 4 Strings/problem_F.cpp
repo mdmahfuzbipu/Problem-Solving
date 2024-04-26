6e67b166053e156b3ee219de8aea4d5886e3904e
mdmahfuzbipu <mdmahfuzbipu@gmail.com>
2024-04-26 11:50:28 +0600

    Problem F string

---
+++ Codeforce/Sheet 4 Strings/problem_F.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+    string S;
+    cin>>S;
+    
+    if(S.length()>10)
+    {
+        cout<<S[0]<<S.length()-2<<S[S.length()-1]<<endl;
+    }
+
+    else
+    {
+        cout<<S<<endl;
+    }
+
+    }
+}
