da9a1b9158c07f88b4795f0fec6c3c2b6d9c1096
mdmahfuzbipu <mdmahfuzbipu@gmail.com>
2024-04-30 17:47:20 +0600

    Problem J

---
+++ Codeforce/Sheet 4 Strings/problem_J.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+int main()
+{
+    string S;
+    cin>>S;
+
+    map<char,int> charCount;
+
+    for(char c:S)
+    {
+        charCount[c]++;
+    }
+
+    for(map<char,int>::iterator i=charCount.begin(); i != charCount.end();i++)
+    {
+        cout<<i->first<<" : "<<i->second<<endl;
+    }
+
+}
